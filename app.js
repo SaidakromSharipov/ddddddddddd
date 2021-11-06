3d3d6a41447b0d25d9457654d9dd067c121bec83
Saidakrom <92370613+SaidakromSharipov@users.noreply.github.com>
2021-11-06 11:06:44 +0300

    ddddddd

---
+++ app.js
@@ -0,0 +1,32 @@
+// "use strict";
+
+let block=document.querySelector('.block');
+
+let block1=$('.block');
+
+block1.hide();
+block1.show();
+
+
+console.log(block);
+console.log(block1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
